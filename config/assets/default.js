4981b19dbd71cd3068d6769ab126e64c56e0424f
Alex Suttmiller <asuttmiller@gmail.com>
2014-11-16 13:02:23 -0500

    Remove socketio support. It's not needed.

--- config/assets/default.js
+++ config/assets/default.js
@@ -37,7 +37,6 @@ module.exports = {
 		allJS: ['gruntfile.js', 'server.js', 'config/**/*.js', 'modules/*/server/**/*.js'],
 		models: 'modules/*/server/models/**/*.js',
 		routes: ['modules/*[!core]/server/routes/**/*.js', 'modules/core/server/routes/**/*.js'],
-		sockets: 'modules/*/server/sockets/**/*.js',
 		config: 'modules/*/server/config/*.js',
 		policies: 'modules/*/server/policies/*.js',
 		views: 'modules/*/server/views/*.html'
